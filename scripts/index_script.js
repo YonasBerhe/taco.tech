d91cf468fe3aefab2b78a0b1bd557486e157e85a
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-10-28 20:16:10 -0700

    fixing styling

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-	$('.parallax-window').parallax({imageSrc: 'http://f.fastcompany.net/multisite_files/fastcompany/poster/2014/11/3038762-poster-p-1-behold-the-taco-emoji.jpg'});
+	$('.parallax-window').parallax();
+	// $('.parallax-window').parallax({imageSrc: 'http://f.fastcompany.net/multisite_files/fastcompany/poster/2014/11/3038762-poster-p-1-behold-the-taco-emoji.jpg'});
 });
