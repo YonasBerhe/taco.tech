4733bac6c9ccdd8242debd0ce3598d3cc4245fd6
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-10-28 19:41:54 -0700

    new index

---
+++ index_script.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$('.parallax-window').parallax({imageSrc: 'http://f.fastcompany.net/multisite_files/fastcompany/poster/2014/11/3038762-poster-p-1-behold-the-taco-emoji.jpg'});
+});
